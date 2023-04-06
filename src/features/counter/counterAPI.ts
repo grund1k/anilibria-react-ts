be97826bfc907ca4d7775f5311f7cfd0b3820ac1
Ivan Komissarov <65980847+grund1k@users.noreply.github.com>
2023-04-07 01:57:21 +0300

    Updates

--- src/features/counter/counterAPI.ts
+++
@@ -1,5 +0,0 @@
-// A mock function to mimic making an async request for data
-export default function fetchCount(amount = 1) {
-  // eslint-disable-next-line max-len, no-promise-executor-return
-  return new Promise<{ data: number }>((resolve) => setTimeout(() => resolve({ data: amount }), 500));
-}
